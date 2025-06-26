25284c4554ce5169378fe333cd4ac4db065df736
ejnhacker <105592400+ejnhacker@users.noreply.github.com>
2025-06-26 13:36:19 +0530

    Update and rename config.py to Space

--- config.py
+++
@@ -1,7 +0,0 @@
-# config.py
-BOT_TOKEN = "your token"
-
-PAIR_LIST = ["EURUSD", "GBPUSD", "USDJPY", "AUDCAD", "EURJPY"]
-TIMEFRAME = "1m"
-SIGNAL_INTERVAL = 180  # Every 3 mins
-CONFIDENCE_THRESHOLD = 99.0  # Ultra accurate signal only
